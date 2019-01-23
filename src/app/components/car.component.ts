826b61cb29a9612fe2db55145afdefc0e561c3c8
unknown <timpriadko@gmail.com>
2019-01-23 13:14:02 +0200

    homework

---
+++ src/app/components/car.component.ts
@@ -0,0 +1,31 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-car',
+    templateUrl: './car.component.html',
+    styleUrls: ['./car.component.css']
+})
+export class CarComponent {
+    carInfo = {
+        name: 'Lexus',
+        mileage: 20000,
+        fuelTankCapacity: 93,
+        currentFuelAmount: 70,
+        characteristics: [
+            'Двигатель 5.7 литра',
+            'Максимальная скорость 220',
+            'Разгон до 100 км/ч за 7.3с'
+        ]
+    };
+
+    driveOnClickHandler($event) {
+            this.carInfo.currentFuelAmount -= 10;
+            if (this.carInfo.currentFuelAmount > 0) {
+                this.carInfo.mileage += 100;
+            }
+    }
+
+    refuelOnClickHandler($event) {
+        this.carInfo.currentFuelAmount = 93;
+    }
+}
